4521e61a59eca2dc83617198db7b1705d2b20aeb
ekisler <kisler.fullstack@gmail.com>
2025-04-18 16:40:06 -0400

    Eliminando Next-SEO y utilizando correctamente metada en next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,30 @@
-import { montserrat } from "./ui/fonts"
+export const metadata = {
+  title: "Learn-n8n",
+  description: "Automatización de procesos con n8n.",
+  openGraph: {
+    type: "website",
+    locale: "es_ES",
+    url: "https://learn-n8n.vercel.app/",
+    site_name: "Learn-n8n",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dxfksb8ua/image/upload/v1744998918/images/bfd888fb01d5421c6bf2c1e19610b97f_rg4gjd.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Learn n8n",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@ekracing",
+    site: "@ekracing",
+    cardType: "summary_large_image",
+  },
+};
+
+import { montserrat } from "./ui/fonts";
 import "@/app/ui/global.css";
-import { ConfigProvider } from 'antd'; 
-import { DefaultSeo } from "next-seo";
-import SEO from "../next-seo.config";
+import { ConfigProvider } from "antd";
 
 export default function RootLayout({
   children,
@@ -11,16 +33,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <DefaultSeo {...SEO} />
-      </head>
       <body className={`${montserrat.className} antialiased`}>
-        <ConfigProvider>
-        {children}
-        </ConfigProvider>
+        <ConfigProvider>{children}</ConfigProvider>
         <footer className="py-10 flex justify-center items-center">
           Hecho con ❤️ por Emisael Kisler
-        </footer> 
+        </footer>
       </body>
     </html>
   );
